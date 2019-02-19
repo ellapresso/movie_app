bd2acdb12c9c9bc136e971b4d4e5199accb25ba5
ellapresso <ellapresso@gmail.com>
2019-02-19 17:29:29 +0900

    Updates

---
+++ precache-manifest.cdd41fee5799754a0d4a8bcf427cb7b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "214a5de8421acef2b1de",
+    "url": "/movie_app/static/js/main.214a5de8.chunk.js"
+  },
+  {
+    "revision": "5178fb6fe39fb9e436b0",
+    "url": "/movie_app/static/js/2.5178fb6f.chunk.js"
+  },
+  {
+    "revision": "214a5de8421acef2b1de",
+    "url": "/movie_app/static/css/main.a865a09b.chunk.css"
+  },
+  {
+    "revision": "36436b029f6d4dedfaecd0f77ee4a29f",
+    "url": "/movie_app/index.html"
+  }
+];
